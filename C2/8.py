4b27f4984064376a79ffa1db4a7f0e473111afc4
dooleys23 <dooleys23@gmail.com>
2018-09-19 22:42:15 -0400

    Adding sample files, source code from wiley textbook
    Signed-off-by: dooleys23 <dooleys23@gmail.com>

--- C2/8.py
+++ C2/8.py
@@ -6,7 +6,7 @@ import code
 class CreditCard:
     """A consumer credit card."""
 
-    def __init__(self, customer, bank, acnt, limit):
+    def __init__(self, customer, bank, acnt, limit,*):
         """Create a new credit card instance.
         The initial balance is zero.
         customer  the name of the customer (e.g., 'John Bowman')
@@ -18,7 +18,9 @@ class CreditCard:
         self._bank = bank
         self._account = acnt
         self._limit = limit
-        self._balance = 0
+        self._bonus = bonus
+        self._balance = 0 + self._bonus
+
 
     def get_customer(self):
         """Return name of the customer."""
@@ -66,5 +68,22 @@ class CreditCard:
         else:
             raise TypeError('A non acceptable input was entered. Please enter a positive amount.')
 #cc is a constructor, as it calls to the <__init__> function
-cc = CreditCard( 'John Doe', '1st Bank' , '5391 0375 9387 5309' , 1000)
-code.interact(local=locals())
+
+def main():
+    # cc = CreditCard( 'John Doe', '1st Bank' , '5391 0375 9387 5309' , 1000, bonus)
+
+    if __name__ == '__main__':
+        wallet = []
+        wallet.append(CreditCard('John Bowman', 'California Savings',
+                                 '5391 0375 9387 5309', 2500))
+        wallet.append(CreditCard('John Bowman', 'California Federal',
+                                 '3485 0399 3395 1954', 3500))
+        wallet.append(CreditCard('John Bowman', 'California Finance',
+                                 '5391 0375 9387 5309', 5000))
+
+        for val in range(1, 17):
+            wallet[0].charge(val)
+            wallet[1].charge(2 * val)
+            wallet[2].charge(3 * val)
+
+main()
